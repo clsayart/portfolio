61ff88b22570bec326fa75a4e3ff8d1f10b67594
Cl Sayart <clsayart@gmail.com>
2021-05-09 12:40:20 +0200

    second commit

---
+++ javascript.js
@@ -0,0 +1,20 @@
+function copyTextToClipboard(text) {
+	if (!navigator.clipboard) {
+	  fallbackCopyTextToClipboard(text);
+	  return;
+	}
+	navigator.clipboard.writeText(text).then(function() {
+	  console.log('Async: Copying to clipboard was successful!');
+	}, function(err) {
+	  console.error('Async: Could not copy text: ', err);
+	});
+  }
+  
+  var copyBobBtn = document.querySelector('.mail');
+	
+  
+  copyBobBtn.addEventListener('click', function(event) {
+	copyTextToClipboard('clsayart@gmail.com');
+  });
+
+console.log("coucou toi")
