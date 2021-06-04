0e8f6eb4cd053a47de0a6722aa259931bf3be666
Cl Sayart <clsayart@gmail.com>
2021-06-04 21:28:30 +0200

    pokedex

--- javascript.js
+++ javascript.js
@@ -23,15 +23,15 @@ copyBobBtn.addEventListener("click", function (event) {
 //SHOW MODAL WITH LINKS TO MY PROJECTS
 
 var linkBtn = document.querySelector(".project-video");
+var modal = document.querySelector("#modal");
 
 linkBtn.addEventListener("click", function (event) {
-	console.log("remove hidden")
 	console.log("remove hidden")
   modal.classList.remove("hidden");
 });
 
 var closeBtn = document.querySelector(".close-button");
-var modal = document.querySelector("#modal");
+
 
 closeBtn.addEventListener("click", () => {
   modal.classList.add("hidden");
@@ -39,16 +39,31 @@ closeBtn.addEventListener("click", () => {
 
 
 
-
 var linkBtn2 = document.querySelector(".project-video2");
+var modal2 = document.querySelector("#modal2");
 
 linkBtn2.addEventListener("click", function (event) {
-  copyDiv.classList.remove("hidden");
+  modal2.classList.remove("hidden");
 });
 
-var closeBtn = document.querySelector(".close-button2");
-var copyDiv = document.querySelector("#modal2");
+var closeBtn2 = document.querySelector(".close-button2");
 
-closeBtn.addEventListener("click", () => {
-  copyDiv.classList.add("hidden");
+
+closeBtn2.addEventListener("click", () => {
+  modal2.classList.add("hidden");
 });
+
+var linkBtn3 = document.querySelector(".project-video3");
+var modal3 = document.querySelector("#modal3");
+
+linkBtn3.addEventListener("click", function (event) {
+  modal3.classList.remove("hidden");
+});
+
+var closeBtn3 = document.querySelector(".close-button3");
+
+
+closeBtn3.addEventListener("click", () => {
+  modal3.classList.add("hidden");
+});
+
